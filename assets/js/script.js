03bab8caeb84a228f8b08a5adfa5c087fadb7ed9
englburc <teacher.levan@gmail.com>
2024-01-18 20:45:17 +0000

    script.js: hooked line-chart id to variable and passed it to the function, changed the variable bame from lineChart to line-chart, added lineTention paramiter to 0.5 to make the line smoother

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
-var zero;
 
-// console.log(`ding`)
 
-const lineChartEl = document.getElementById('lineChart');
 
+
+const lineChartEl = document.getElementById('line-chart');
+
+// test data
 const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
 const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
 
 function drawLineChart(xValues, yValues) {
-    new Chart("lineChart", {
+    new Chart(lineChartEl, {
         type: "line",
         data: {
             labels: xValues,
             datasets: [{
                 fill: false,
-                lineTension: 0,
+                lineTension: 0.9,
                 backgroundColor: "rgba(0,0,255,1.0)",
                 borderColor: "rgba(0,0,255,0.1)",
-                data: yValues
+                data: yValues,
+                // popointRadius: 0
             }]
         },
         options: {
