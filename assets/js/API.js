6dee163a46f9628b40779cae817e312b0fbf3568
IgorTraspadini <igorchieppe@hotmail.com>
2024-01-24 07:43:15 -0300

    some asjustment on CSS (fonf-size, adjust content on the page, margin and padding, etc..)

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,5 +1,5 @@
 const APIKey = '3be0c4ff92mshf3ed26e5ff73578p1884ecjsn26894e084b86';
-const APIKey2 = "TcSAvBTZl318Z8WuUz8XSJQFRnjScxv6";
+const APIKey2 = "5ab31f1e6c6bcfdebd17fc608c2d7123";
 // Use it just in the appresentation
 //const APIKey2 = "g2PtREXHUtUFvq9TqT2kXHdmXdL3gQ0n";
 
@@ -44,14 +44,14 @@ function fundamentalData(thicker) {
       ElemFDH.empty();
       ElemFDR.empty();
       ElemFDL.empty();
-      const compName = $("<p class='p-2 fs-5 border-2 rounded w-50'>").text(`${data[0].companyName} (${data[0].symbol})`);
-      const price = $("<span class='p-2 fs-5 border-2 rounded w-40'>").text(`  ${data[0].price} (${(((data[0].price / (data[0].price - data[0].changes)) - 1) * 100).toFixed(2)}%)`);
+      const compName = $("<p class='p-2 fs-5 border-2 rounded'>").text(`${data[0].companyName} (${data[0].symbol})`);
+      const price = $("<span class='p-2 fs-5 border-2 rounded'>").text(`  ${data[0].price} (${(((data[0].price / (data[0].price - data[0].changes)) - 1) * 100).toFixed(2)}%)`);
       ElemFDH.append(price);
 
-      const beta = $("<p class='m-2 pb-2 fs-6 border-bottom'>").text(`Beta: ${data[0].beta}`);
-      const volume = $("<p class='m-2 pb-2 fs-6 border-bottom'>").text(`Volume: ${data[0].volAvg}`);
-      const dayRange = $("<p class='m-2 pb-2 fs-6 border-bottom'>").text(`Day Range: ${data[0].range}`);
-      const open = $("<p class='m-2 pb-2 fs-6 border-bottom'>").text(`Open: ${(data[0].price - data[0].changes).toFixed(1)}`);
+      const beta = $("<p class='m-2 pb-2 border-bottom' style='font-size: smaller'>").text(`Beta: ${data[0].beta}`);
+      const volume = $("<p class='m-2 pb-2 border-bottom' style='font-size: smaller'>").text(`Volume: ${data[0].volAvg}`);
+      const dayRange = $("<p class='m-2 pb-2 border-bottom' style='font-size: smaller'>").text(`Day Range: ${data[0].range}`);
+      const open = $("<p class='m-2 pb-2 border-bottom' style='font-size: smaller'>").text(`Open: ${(data[0].price - data[0].changes).toFixed(1)}`);
 
       ElemFDH.append(compName, price);
       ElemFDR.append(volume, open, dayRange, beta);
@@ -65,7 +65,7 @@ function fundamentalData(thicker) {
       return response.json();
     })
     .then(function (data) {
-      const MarketCap = $("<p class='m-2 pb-2 fs-6 border-bottom'>").text("Market Cap: " + data[0].marketCapTTM.toLocaleString('en-US', {
+      const MarketCap = $("<p class='m-2 pb-2 border-bottom' style='font-size: smaller'>").text("Market Cap: " + data[0].marketCapTTM.toLocaleString('en-US', {
         // add suffixes for thousands, millions, and billions
         // the maximum number of decimal places to use
         maximumFractionDigits: 2,
@@ -73,20 +73,14 @@ function fundamentalData(thicker) {
         notation: 'compact',
         compactDisplay: 'short'
       }));
-      const EnterpriseValue = $("<p class='m-2 pb-2 fs-6 border-bottom'>").text("Enterprise Value: " + data[0].enterpriseValueTTM.toLocaleString('en-US', {
+      const EnterpriseValue = $("<p class='m-2 pb-2 border-bottom' style='font-size: smaller'>").text("Enterprise Value: " + data[0].enterpriseValueTTM.toLocaleString('en-US', {
         maximumFractionDigits: 2,
         notation: 'compact',
         compactDisplay: 'short'
       }));
-      const peRatio = $("<p class='m-2 pb-2 fs-6 border-bottom'>").text(`PE Ratio: ${data[0].peRatioTTM.toFixed(2)}`);
-      const dividend = $("<p class='m-2 pb-2 fs-6 border-bottom'>").text(`Dividend: $${data[0].dividendPerShareTTM} (${data[0].dividendYieldPercentageTTM.toFixed(2)}%)`);
+      const peRatio = $("<p class='m-2 pb-2 border-bottom' style='font-size: smaller'>").text(`PE Ratio: ${data[0].peRatioTTM.toFixed(2)}`);
+      const dividend = $("<p class='m-2 pb-2 border-bottom' style='font-size: smaller'>").text(`Dividend: $${data[0].dividendPerShareTTM} (${data[0].dividendYieldPercentageTTM.toFixed(2)}%)`);
       ElemFDL.append(MarketCap, EnterpriseValue, peRatio, dividend);
-
-
-    //  console.log("Revenue: ");
-    //  console.log("Net Income: ");
-    //  console.log("PE Ratio: " + data[0].peRatioTTM.toFixed(2));
-    //  console.log(`Dividend: $${data[0].dividendPerShareTTM} (${data[0].dividendYieldPercentageTTM.toFixed(2)}%)`)
 
       console.log(data);
     })
