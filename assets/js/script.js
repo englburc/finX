f97ad2b103023d78f4912ffdbc0c0bcb103e5655
IgorTraspadini <igorchieppe@hotmail.com>
2024-01-24 10:29:19 -0300

    Add stock thicker on localStore and render the buttoms on the History section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,17 @@
 window.onload = function () {
   const ElemForm = $("#stock-form");
   const ElemInp = $("#ticker-code");
+  const Elemhistory = $("#history-section");
 
   ElemForm.on("submit", function (e) {
     e.preventDefault();
     const thicker = ElemInp.val().trim();
     if (thicker !== "") {
-      console.log("submitted");
-      renderGraph(thicker);
-      fundamentalData(thicker);
-      forexPrice();
-      indexPrice();
+//      renderGraph(thicker);
+//      fundamentalData(thicker);
+//      forexPrice();
+//      indexPrice();
+      addHistory(thicker);
       ElemInp.val("");
     }
     else {
@@ -19,15 +20,58 @@ window.onload = function () {
     }
 
   })
+
+  renderButtons();
+
   $('#timer').text(dayjs().format('YYYY-MM-DD HH:mm'));
 
   function showtime() {
     const timeInterval = setInterval(function () {
-
       $('#timer').text(dayjs().format('YYYY-MM-DD HH:mm'));
     }, 60000);
   }
   showtime()
+
+
+  // Function to render the buttons on the history section.
+  // If called with parameter, it render just a new button
+  // If called without parameter, it render a new button for each stock in the history array     
+  function renderButtons(stock) {
+    if (stock === undefined) {
+      historyStorage = JSON.parse(localStorage.getItem("history_stock"));
+      if (historyStorage !== null) {
+        $.each(historyStorage, function (i, v) {
+          const ElemButton = $("<button class='m-2 p-2 fs-5 border-2 rounded bg-primary-subtle'>").attr("type", "button");
+          ElemButton.attr("data-stock", v);
+          ElemButton.text(v);
+          Elemhistory.prepend(ElemButton);
+        })
+      }
+    }
+    else {
+      const ElemButton = $("<button class='m-2 p-2 fs-5 border-2 rounded bg-primary-subtle'>").attr("type", "button");
+      ElemButton.attr("data-stock", stock);
+      ElemButton.text(stock);
+      Elemhistory.prepend(ElemButton);
+    }
+  }
+
+  // function to add a new stock to the history Object in the localStorage.
+  // If the localStorage is empty, it create a new array with the stock passed to the function and add it to the localStorage
+  // If not, it push a new stock to the localStorage only in case that it doesn't exist.
+  function addHistory(stock) {
+    historyStorage = JSON.parse(localStorage.getItem("history_stock"));
+    if (historyStorage === null) {
+      historyStorage = [stock];
+      localStorage.setItem("history_stock", JSON.stringify(historyStorage));
+      renderButtons(stock);
+    }
+    else if (!historyStorage.includes(stock)) {
+      historyStorage.push(stock);
+      localStorage.setItem("history_stock", JSON.stringify(historyStorage));
+      renderButtons(stock);
+    }
+  }
 
 }
 
